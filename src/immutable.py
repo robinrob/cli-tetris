457b9565c410874194eae5e03fd7725c83380ac8
Robin Smith <robin@rsmith.io>
2018-05-27 13:04:17 +0100

    Rename Immutable.thing to Immutable._attrs_dict

--- src/immutable.py
+++ src/immutable.py
@@ -6,22 +6,22 @@ class Immutable(object):
 
     def __init__(self, attrs_dict=None):   # noqa: D107
         if attrs_dict is not None:
-            object.__setattr__(self, 'thing', attrs_dict)
+            object.__setattr__(self, '_attrs_dict', attrs_dict)
         else:
-            object.__setattr__(self, 'thing', {})
+            object.__setattr__(self, '_attrs_dict', {})
 
     # Setting is prevented if attribute is already set
     def __setattr__(self, attr, val):
         """Set attribute value if not already set."""
-        if attr in object.__getattribute__(self, 'thing'):
+        if attr in object.__getattribute__(self, '_attrs_dict'):
             raise AttributeError(f"Attribute '{attr}' of {self.__class__.__name__} object is not writable'")
-        object.__getattribute__(self, 'thing')[attr] = val
+        object.__getattribute__(self, '_attrs_dict')[attr] = val
 
     # None is returned if attr is not set
     def __getattribute__(self, attr):
         """Get attribute value."""
-        if attr in object.__getattribute__(self, 'thing'):
-            return object.__getattribute__(self, 'thing')[attr]
+        if attr in object.__getattribute__(self, '_attrs_dict'):
+            return object.__getattribute__(self, '_attrs_dict')[attr]
 
         else:
             try:
@@ -32,4 +32,4 @@ class Immutable(object):
 
     def to_dict(self):
         """Get dictionary representation of this object's attributes."""
-        return object.__getattribute__(self, 'thing')
+        return object.__getattribute__(self, '_attrs_dict')
